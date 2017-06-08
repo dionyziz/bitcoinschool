dca1aa0f8bcaf5b0fcbf67f1a3d768a8ff12c1dd
Dionysis Zindros <dionyziz@gmail.com>
2017-06-08 18:10:19 +0300

    Force CSS uncaching

--- law/index.php
+++ law/index.php
@@ -27,7 +27,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="assets/css/main.css?version=1" />
         <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
         <link rel="icon" type="image/png" href="images/favicon.png" />
     </head>
